eac8ef97ea7430867e72728ac54e5af8dd6551b2
Dennis Lee <dennis@gsfcorp.com>
2025-08-19 02:20:54 +0800

    feat: Implement initial classes for object detection, speed, tracking, and utility functions 🎥🚀
    - Added `ObjectDetection` class with methods for detecting objects in frames and generating random colors.
    - Introduced `ObjectSpeed` class to update and calculate the speed of detected objects.
    - Created `MultiThreadingTracker` class for managing threading in object tracking.
    - Added `ocsort` class for updating trackers with new detections.
    - Implemented utility functions for calculating meters per pixel, generating wrap matrices, and applying perspective warps.
    
    These additions lay the groundwork for enhanced object detection and tracking capabilities in the application.

---
+++ engine/object_tracking.py
@@ -0,0 +1,16 @@
+class MultiThreadingTracker:
+    def __init__(self):
+        pass
+
+    def start_cap_thread(self, input):
+        pass
+
+    def start_detection_thread(
+        self, model_path, device, batch_size: int, img_size: int
+    ):
+        pass
+
+
+class MultiObjectTracking:
+    def __init__(self):
+        pass
